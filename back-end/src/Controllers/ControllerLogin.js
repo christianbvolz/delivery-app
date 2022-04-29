2b0014be44ad226a12be539bf6626b25ad8db280
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-04-29 17:49:56 -0300

    verificando a senha do usuario

--- back-end/src/Controllers/ControllerLogin.js
+++ back-end/src/Controllers/ControllerLogin.js
@@ -2,9 +2,9 @@ const service = require('../Services/ServiceLogin');
 const { generateToken } = require('../Token');
 
 const getLogin = async (req, res, next) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   
-  const responseUser = await service.getLogin(email);
+  const responseUser = await service.getLogin(email, password);
   
   if (!responseUser) {
     return next({ error: 400, message: 'User does not exist' });
@@ -13,7 +13,7 @@ const getLogin = async (req, res, next) => {
   const { dataValues: { id, email: userEmail } } = responseUser;
   const token = generateToken({ id, userEmail });
 
-  return res.status(200).json(token);
+  return res.status(200).json({ token });
 };
 
 module.exports = {
