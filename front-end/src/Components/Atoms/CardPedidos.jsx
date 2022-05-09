9529ef2c3549d34dd57dffd332312d14e75ae950
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-05-09 20:01:19 -0300

    criando os cards de pedidos

--- front-end/src/Components/Atoms/CardPedidos.jsx
+++ front-end/src/Components/Atoms/CardPedidos.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function CardPedidos() {
+import '../../Style/Card.css';
+
+function CardPedidos({ item }) {
   return (
-    <div>
-      Pedidos
+    <div className="d-flex flex-row justify-content-between card m-2 me-4 p-2">
+      <div className="">
+        pedido:
+        { item.id }
+      </div>
+      <div>
+        { item.status }
+      </div>
+      <div>
+        <div>
+          { item.saleDate }
+        </div>
+        <div>
+          R$:
+          { item.totalPrice.toFixed(2) }
+        </div>
+      </div>
     </div>
   );
 }
+
+CardPedidos.propTypes = {
+  item: PropTypes.objectOf(PropTypes.objectOf).isRequired,
+};
 
 export default CardPedidos;
--- front-end/src/Pages/Pedidos.jsx
+++ front-end/src/Pages/Pedidos.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { CardPedidos } from '../Components/Atoms';
 import Navegacao from '../Components/Atoms/Navegacao';
-import { ProductsRelatedRequests } from '../Services/request';
+// import { ProductsRelatedRequests } from '../Services/request';
 
 function Pedidos() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [pedidos, setPedidos] = useState([]);
   const requestedData = async () => {
     setLoading(true);
-    const data = await ProductsRelatedRequests('/orders');
-    console.log('Verificando data pedido', data);
+    // const data = await ProductsRelatedRequests('/orders');
+    // MOCANDO O RESULTADO DO BACK END
+    const dataMock = [
+      {
+        id: 1,
+        userId: 3,
+        sellerId: 3,
+        totalPrice: 120,
+        deliveryAdress: 'Rua de teste',
+        deliveryNumber: '233',
+        saleDate: '2022-05-10',
+        status: 'Preparando',
+      },
+      {
+        id: 1,
+        userId: 3,
+        sellerId: 3,
+        totalPrice: 120,
+        deliveryAdress: 'Rua de teste',
+        deliveryNumber: '233',
+        saleDate: '2022-05-10',
+        status: 'Preparando',
+      },
+    ];
+    setPedidos(dataMock);
     setLoading(false);
   };
 
@@ -20,6 +45,7 @@ function Pedidos() {
       <Navegacao />
       <div>
         { loading && <p>Carregando...</p> }
+        { pedidos.map((item) => <CardPedidos key={ item.id } item={ item } />)}
       </div>
     </div>
   );
